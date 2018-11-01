f5ada055076d99ad3b7de9c391b3343ddf453a4b
Guillaume <sagitta1618@gmail.com>
2018-11-01 18:16:50 +0000

    Replace build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-..\pyenv\Scripts\activate.bat
-pyinstaller ui-dir.spec
-cd dist
-7z a -r ui.zip ui
-cd ..
-move dist\ui.zip ui.zip
+..\pyenv\Scripts\activate.bat && ^
+pyinstaller ui-dir.spec && ^
+cd dist && ^
+7z a -r ui.zip ui && ^
+cd .. && ^
+move dist\ui.zip ui.zip && ^
 pyinstaller splashScreen-exe.spec
