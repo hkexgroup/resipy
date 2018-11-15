63254baed8dd34b915d0a51be17be1e355154423
Jimmy P Boyd <j.boyd1@lancaster.ac.uk>
2018-11-15 12:15:36 +0000

    Fleshed out systemCheck() function in meshTools, function now returns info on the OS kernal, RAM and CPU count. It also checks if wine is installed if using linux or mac. (Hopefully).

--- api/meshTools.py
+++ api/meshTools.py
@@ -1601,17 +1601,27 @@ def dat_import(file_path):
     return mesh
 #now do mesh.add_e_nodes to add electrode positions to the mesh. 
     
-#%% ram amount check. 
+
+#%% ram amount check and is wine installed?. 
 #Now for complicated meshes we need alot more RAM. the below function is a os agnostic
 #function for returning the amount of total ram. 
+# we also need to check wine is installed if running on macOs or linux. 
 def systemCheck():
     print("________________System-Check__________________")
     #display processor info
     print("Processor info: %s"%platform.processor())
     num_threads = multiprocessing.cpu_count()
     print("Number of logical CPUs: %i"%num_threads)
+    #this message will display if wine is not installed / detected
+    helpful_msg ="""   
+This version of pyR2 requires wine to run R2.exe, please consider installing
+'wine is not an emulator' package @ https://www.winehq.org/. On linux wine can be found on
+most reprositories (ubuntu/debian users can use "sudo apt install wine"). Wine acts as
+a compatiblity layer between unix like OS systems (ie macOS and linux) and windows programs. 
+    """
+    msg_flag = False
     #check operating system 
-    OpSys=platform.system()
+    OpSys=platform.system()    
     if OpSys=='Darwin':
         print("Kernal type: macOS")
     else:
@@ -1619,6 +1629,15 @@ def systemCheck():
     #check the amount of ram 
     if OpSys=="Linux":
         totalMemory = os.popen("free -m").readlines()[1].split()[1]
+        #detect wine 
+        is_wine = os.popen("wine --version").readlines()#[0].split()[0]
+        if is_wine.find("wine") == -1:
+            warnings.warn("Wine is not installed!", Warning)
+            msg_flag = True
+        else:
+            wine_version = is_wine.split()[0].split('-')[1]
+            print("Wine version = "+wine_version)
+                          
     elif OpSys=="Windows":
         info = os.popen("systeminfo").readlines()
         for i,line in enumerate(info):
@@ -1631,6 +1650,14 @@ def systemCheck():
     elif OpSys=='Darwin':
         totalMemory = os.popen("hwprefs memory_size").readlines()[0].split()[0]
         totalMemory = int(totalMemory)*1000
+        #detect wine 
+        is_wine = os.popen("wine --version").readlines()#[0].split()[0]
+        if is_wine.find("wine") == -1:
+            warnings.warn("Wine is not installed!", Warning)
+            msg_flag = True
+        else:
+            wine_version = is_wine.split()[0].split('-')[1]
+            print("Wine version = "+wine_version)
         
     else:
         raise OSError("unrecognised/unsupported operating system")
@@ -1640,11 +1667,15 @@ def systemCheck():
     
     #print some warnings incase the user has a low end PC
     if totalMemory <= 4000:
-        warnings.warn("The amount of RAM currently installed is quite low (<4Gb), complicated problems may incur memory access voilations")
+        warnings.warn("The amount of RAM currently installed is low (<4Gb), complicated ERT problems may incur memory access voilations", Warning)
     if num_threads <=2:
-        warnings.warn("Your core count is quite small, multithreaded workflows will not perform well.")
+        warnings.warn("Only one or two CPUs detected, multithreaded workflows will not perform well.", Warning)
+    if msg_flag:
+        print(helpful_msg)
     
     return {'memory':totalMemory,'core_count':num_threads,'OS':OpSys}
+
+#info = systemCheck()
     
 #%% test code for borehole quad mesh
 #elec_x = np.append(np.arange(10),[5.1,5.1,5.1])
