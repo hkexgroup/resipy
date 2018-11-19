cb5f4dcebf770e96f8e5a5b0c8035377bcb5eb93
sagitta1618 <sagitta1618@gmail.com>
2018-11-19 15:33:12 +0000

    reorganising files and folders

--- build.bat
+++
@@ -1,7 +0,0 @@
-..\pyenv\Scripts\activate.bat && ^
-pyinstaller -y ui-dir.spec && ^
-cd dist && ^
-7z a -r ui.zip ui && ^
-cd .. && ^
-move dist\ui.zip ui.zip && ^
-pyinstaller -y splashScreen-exe.spec
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ../pyenv/bin/activate
-pyinstaller -y ui-dir.spec
-cd dist
-zip -r ui.zip ui
-cd ..
-mv dist/ui.zip ui.zip
-pyinstaller -y splashScreen-exe.spec
