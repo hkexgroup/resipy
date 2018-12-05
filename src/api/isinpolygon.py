943005103d2420bb4d097f0a36cb6a0ad50cdbe7
Jimmy P Boyd <j.boyd1@lancaster.ac.uk>
2018-12-05 15:07:59 +0000

    Towards 3D, isinpolygon now has an isinvolume() function.

--- src/api/isinpolygon.py
+++ src/api/isinpolygon.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct  9 15:11:01 2018, python 3.6.6
-vectorised isinpolygon algorithm using numpy, ask jamyd91 for a pyx version, it may give a slight performance boost. 
+vectorised isinpolygon/isinvolume algorithm using numpy
 @author: jamyd91
 """
 import numpy as np
@@ -33,8 +33,8 @@ def isinpolygon(x,y,poly_data,ray_cast=10000):
         determines how the far to the right a point is ray casted 
     Returns
     ----------
-    ans: boolian, array like
-        true indexes where point is inside polygon 
+    inside: boolian, numpy array 
+        true indexes where point is inside volume
     Notes
     ----------
     polyx: array like
@@ -77,10 +77,93 @@ def isinpolygon(x,y,poly_data,ray_cast=10000):
         #1 indicates the point is inside the polygon 
         
         #NB: if a you have complex geometry the number of times the point is counted inside a polygon will be more, 
-        # say a star. Rule of thumb is that the number of counts should be odd.
+        # say a star. Rule is that the number of counts should be odd.
         
     return count%2 != 0 #returns boolian indexes
 
-def planeintersect(x,y,z,poly_x,poly_y,poly_z,ray_cast=float('inf')):
-    pass
+#%% determine if line intersects plane 
+def planeintersect(Ax, Ay, Az, 
+                   Bx, By, Bz,
+                   plane_x,
+                   plane_y,
+                   plane_z):#pass all variables as arguments so there is no need for unpacking 
+    """
+    Does lines AB intersect a plane in 3D coordinates 
+    """
+    count=np.zeros_like(Ax, dtype = int)#number of times an intersection has been detected 
+    intersect = np.zeros_like(Ax, dtype=int)
+    for i in range(len(plane_x)):
+        if i==len(plane_x)-1:#its the last iteration
+            Cx=plane_x[-1]
+            Cy=plane_y[-1]#take last and first planegon point
+            Cz=plane_z[-1]
+            Dx=plane_x[0]
+            Dy=plane_y[0]
+            Dz=plane_z[0]
+        else:
+            Cx=plane_x[i]
+            Cy=plane_y[i]
+            Cz=plane_z[i]
+            Dx=plane_x[i+1]
+            Dy=plane_y[i+1]
+            Dz=plane_z[i+1]
+        #line CD will represent a segment from the planegon
+        idxY = (ccw(Ax,Ay,Bx,By,Cx,Cy) != ccw(Ax,Ay,Bx,By,Dx,Dy)) & (ccw(Cx,Cy,Dx,Dy,Ax,Ay) != ccw(Cx,Cy,Dx,Dy,Bx,By))
+        idxZ = (ccw(Ax,Az,Bx,Bz,Cx,Cz) != ccw(Ax,Az,Bx,Bz,Dx,Dz)) & (ccw(Cx,Cz,Dx,Dz,Ax,Az) != ccw(Cx,Cz,Dx,Dz,Bx,Bz))
+        count[idxY]+=1 # add one to the indexes where true
+        count[idxZ]+=1 # add one to the indexes where true
+        
+    hit = np.argwhere(count>1)
+    intersect[hit]=1 
+    
+    return intersect # return binary 
+
+#%% is inside a 
+def isinvolume(x,y,z,volume_data,ray_cast=10000):
+    """
+    Determine if a point lies inside a bounding volume 
+    Parameters
+    ----------
+    x: array like, float
+        x coordinate of query point
+    y: array like, float
+        y coordinate of query point 
+    z: array like, float
+        z coordinate of query point
+    volume_data: list 
+        contains column of poly_data, in the form (polyx, polyy, polyz)
+    ray_cast: float, optional
+        determines how the far in the x axis a point is ray casted 
+    Returns
+    ----------
+    inside: boolian, numpy array 
+        true indexes where point is inside volume
+    Notes
+    ----------
+    polyx: array like
+        polygon x coordinates 
+    polyy: array like
+        polygon y coordinates
+    """
+    if not isinstance(volume_data,list):
+        raise TypeError("Expected list type argument for 'volume_data'")
+    Ax=np.array(x) # make coordinates into numpy arrays 
+    Ay=np.array(y)
+    Az=np.array(z)
+    num_faces=len(volume_data)
+    hit_array=np.zeros((num_faces,len(x)))
+    #sort lines AB
+    Bx = Ax + ray_cast
+    for i in range(num_faces):
+        polyx = volume_data[i][0]#retrive face data
+        polyy = volume_data[i][1]
+        polyz = volume_data[i][2]
+        hits = planeintersect(Ax,Ay,Az,Bx,Ay,Az,polyx,polyy,polyz) # find 0 1 array of hits 
+        hit_array[i,:]=hits#append into result array
+    
+    count = np.sum(hit_array,0)#sum results along the horizontal axis
+    
+    #the number of intersects with a plane should be  odd   
+    return count%2 != 0 #returns boolian indexes
+        
     
