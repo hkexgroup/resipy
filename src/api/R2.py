34480a3109699d5fe30bd33f17b63ef056ef6b2c
Jimmy B <j.boyd1@lancaster.ac.uk>
2019-01-08 19:50:15 +0000

    Changed test  code at bottom of R2.py for 3D testing. Uncomment for 3d testing

--- src/api/R2.py
+++ src/api/R2.py
@@ -2102,29 +2102,13 @@ def pseudo(array, resist, spacing, label='', ax=None, contour=False, log=True, g
 
 
 #%% 3D testing
-<<<<<<< HEAD
 #k = R2(typ='R3t')
-#k.createSurvey('api/test/protocol3D.dat', ftype='Protocol')
-#elec = np.genfromtxt('api/test/electrodes3D.dat')
+#k.createSurvey('test/protocol3D.dat', ftype='Protocol')
+#elec = np.genfromtxt('test/electrodes3D.dat')
 #k.setElec(elec)
-#k.createMesh(cl=10) # it runs but vtk and dat are size 0 (mesh related issue)
-#k.mesh.write_vtk(os.path.join(k.dirname, 'mesh.vtk'))
+#k.createMesh(cl=4) # it runs but vtk and dat are size 0 (mesh related issue)
 #k.invert()
-#k.showResults()
-||||||| merged common ancestors
-#k = R2(typ='R3t')
-#k.createSurvey('api/test/protocol3D.dat', ftype='Protocol')
-#elec = np.genfromtxt('api/test/electrodes3D.dat')
-#k.setElec(elec)
-#k.createMesh(cl=20) # it runs but vtk and dat are size 0 (mesh related issue)
-#k.invert()
-#k.showResults()
-=======
-k = R2(typ='R3t')
-k.createSurvey('test/protocol3D.dat', ftype='Protocol')
-elec = np.genfromtxt('test/electrodes3D.dat')
-k.setElec(elec)
-k.createMesh(cl=4) # it runs but vtk and dat are size 0 (mesh related issue)
-k.invert()
-k.showResults()
->>>>>>> f023dc009f404fdf2380cb63529405516e558410
+##k.showResults()# only works for 2D currently 
+#mesh = mt.vtk_import('invdir/f001.vtk')
+#mesh.show()
+
