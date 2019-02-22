6c16a72d4402219e975173fa0d031676f9890d6b
Boyd <j.boyd1@lancaster.ac.uk>
2019-02-22 13:20:05 +0000

    Added some lines to getResults to add survey title to mesh results

--- src/api/R2.py
+++ src/api/R2.py
@@ -1647,6 +1647,7 @@ class R2(object): # R2 master class instanciated by the GUI
                 fresults = os.path.join(self.dirname, 'ref', 'f001_res.vtk')
             print('reading ref', fresults)
             mesh = mt.vtk_import(fresults)
+            mesh.mesh_title = self.surveys[0].name
             mesh.elec_x = self.elec[:,0]
             mesh.elec_y = self.elec[:,1]
             mesh.elec_z = self.elec[:,2]
@@ -1667,9 +1668,10 @@ class R2(object): # R2 master class instanciated by the GUI
             if os.path.exists(fresults):
                 print('reading ', fresults)
                 mesh = mt.vtk_import(fresults)
-                mesh.elec_x = self.elec[:,0]
-                mesh.elec_y = self.elec[:,1]
-                mesh.elec_z = self.elec[:,2]
+                mesh.mesh_title = self.surveys[i+1].name
+                mesh.elec_x = self.surveys[i+1].elec[:,0]
+                mesh.elec_y = self.surveys[i+1].elec[:,1]
+                mesh.elec_z = self.surveys[i+1].elec[:,2]
                 self.meshResults.append(mesh)
             else:
                 break
