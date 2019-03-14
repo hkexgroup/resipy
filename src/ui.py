50628537c7440ceccdba9af488b471454d3d3a3e
Guillaum <sagitta1618@gmail.com>
2019-03-14 20:31:26 +0000

    disable LME in ui as it requires a tweaked version of statsmodels

--- src/ui.py
+++ src/ui.py
@@ -1684,9 +1684,9 @@ class App(QMainWindow):
             elif index == 2:
                 mwFitError.plot(self.r2.pwlfit)
                 self.r2.err = True
-            elif index == 3:
-                mwFitError.plot(self.r2.lmefit)
-                self.r2.err = True
+#            elif index == 3:
+#                mwFitError.plot(self.r2.lmefit)
+#                self.r2.err = True
             else:
                 print('NOT IMPLEMENTED YET')
             if index == 0:
@@ -1704,7 +1704,7 @@ class App(QMainWindow):
         errFitType.addItem('Observed Errors')
         errFitType.addItem('Linear')
         errFitType.addItem('Power-law')
-        errFitType.addItem('Linear Mixed Effect')
+#        errFitType.addItem('Linear Mixed Effect')
         errFitType.currentIndexChanged.connect(errFitTypeFunc)
         errFitType.setToolTip('Select an error model to use.')
         errorLayout.addWidget(errFitType)
