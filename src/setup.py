a636fbbc0a8b1deeafc9863ceb4f0204ec19a4a7
Guillaum <sagitta1618@gmail.com>
2019-04-16 19:42:54 +0100

    updates files for pypi

--- src/setup.py
+++ src/setup.py
@@ -2,14 +2,15 @@
 import setuptools
 from resipy.R2 import ResIPy_version
 
-with open("../README.md", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ResIPy",
-    version=ResIPy_version,
+    name="resipy",
+#    version=ResIPy_version,
+    version="1.1.9",
     author="HKEx",
-    description="API for ERT inversion (DC/IP)",
+    description="API for resistivity and IP inversion/modelling around R2 codes",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hkex/pyr2",
@@ -18,7 +19,8 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
+
