bf24df4ccb9d45475ca537a98aa086c1191279a8
Guillaum <sagitta1618@gmail.com>
2019-06-19 16:28:55 +0100

    Comment k.computeErrorModel() in test.py

--- src/test.py
+++ src/test.py
@@ -315,7 +315,7 @@ k.createSurvey('resipy/test/protocol3D.dat', ftype='Protocol')
 elec = np.genfromtxt('resipy/test/electrodes3D.csv',delimiter=',')
 k.setElec(elec)
 k.importMesh('resipy/test/mesh3D.vtk')
-k.computeModelError()
+#k.computeModelError()
 #k.write2in()
 #k.param = param
 k.invert()
