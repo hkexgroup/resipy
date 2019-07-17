526f8698e1377c51a131fcc7143ab34398157f33
Guillaum <sagitta1618@gmail.com>
2019-07-17 20:59:04 +0200

    update gitlab-ci to add jupyter gallery

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ author = 'G. Blanchy, S. Saneiyan, J. Boyd, P. McLachlan'
 # The short X.Y version
 version = resipy.ResIPy_version
 # The full version, including alpha/beta/rc tags
-release = 'v0.1'
+release = resipy.ResIPy_version
 
 
 # -- General configuration ---------------------------------------------------
