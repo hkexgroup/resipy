600effad5686054cafc1fe22c043698a6ec0d7a0
U-SINA-OFFICE\Sina <s.sanei@gmail.com>
2019-08-27 14:18:31 -0400

    update IP part of paperExamples.py

--- src/paperExamples.py
+++ src/paperExamples.py
@@ -354,18 +354,21 @@ fig.show()
 #%% IP Calcite precipitation inversion (Rifle, CO, USA)
 k = R2(typ = 'cR2') # initiate an R2 instance (considering there is IP data in the input data)
 k.createSurvey('./resipy/test/IP/IP_MICP_all.csv', ftype='Syscal') # import data
-k.filterRecip(percent=20) # remove\ing datapoints with > 20% reciprocal error
+k.filterRecip(percent=5) # remove\ing datapoints with > 20% reciprocal error
 k.removenested() # removing nested measurements
-k.iprangefilt(0,25) # setting phase shift range to 0 < -ϕ < 25
-k.pwlfit() # adding resistance power-law error model to data
-k.plotIPFitParabola() # adding phase power-law error model to data
+k.iprangefilt(0,20) # setting phase shift range to 0 < -ϕ < 25
+k.linfit()
+k.plotIPFit()
+#k.pwlfit() # adding resistance power-law error model to data
+#k.plotIPFitParabola() # adding phase power-law error model to data
 k.err = True # using error models (DC and IP) - automatically done in the GUI when fitting the error model
 k.createMesh(typ='trian') # create triangular mesh
 k.param['a_wgt'] = 0 # "a_wgt" = 0 when there is individual resistance error
 k.param['b_wgt'] = 0 # "b_wgt" = 0 when there is individual phase error
-k.param['tolerance'] = 1.2 # based on data, field site and experience
+k.param['tolerance'] = 1.15 # based on data, field site and experience
+k.param['min_error'] = 0.001
 k.invert() # run the inversion (and write cR2.in and protocol.dat automatically)
-k.showResults(attr='Sigma_real(log10)') # show the inverted real conductivity section
+k.showResults(attr='Magnitude(Ohm.m)') # show the inverted real conductivity section
 k.showResults(attr='Phase(mrad)') # show the inverted phase shift section
 
 #%% graph section
