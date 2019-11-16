e236d17d2a606da3aeb1553b6ce39741493d95a6
Sina Saneiyan <s.sanei@gmail.com>
2019-11-16 15:01:42 -0500

    fix issue #275 - custom sequence now reads all data and requires headers

--- src/ui.py
+++ src/ui.py
@@ -3208,6 +3208,8 @@ combination of multiple sequence is accepted as well as importing a custom seque
                 self.rmBtn.deleteLater()
                 self.importBtn.deleteLater()
                 self.deleteLater()
+                if self.seq == 'custSeq':
+                    self.fname = ''
             
             def getData(self):
                 if self.seq == 'custSeq':
@@ -3238,7 +3240,7 @@ combination of multiple sequence is accepted as well as importing a custom seque
            'wenner': '<img height=140 src="%s">' % Wenner,
            'schlum1': '<img height=140 src="%s">' % Schlum,
            'multigrad': '<img height=140 src="%s">' % Gradient,
-           'custSeq': 'Use the button to import a custom CSV file (no header)\ncolumn1: C+, column2: C-, column3: P+, column4: P-'
+           'custSeq': 'Use the button to import a custom CSV file (with headers)\ncolumn1: C+, column2: C-, column3: P+, column4: P-'
             }
         
         arrayLabel = QLabel('Sequence help will be display here.')
