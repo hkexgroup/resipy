add electrode scaling (fix #286) and fix bugs from updateElec() in ui.py

src/resipy/meshTools.py
@@ -18,7 +18,7 @@ import time
 #import matplotlib and numpy packages 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.collections import PolyCollection
+from matplotlib.collections import PolyCollection, PatchCollection
 from matplotlib.colors import ListedColormap
 import matplotlib.tri as tri
 from mpl_toolkits.mplot3d import Axes3D
@@ -673,7 +673,10 @@ class Mesh:
         
         if electrodes: #try add electrodes to figure if we have them 
             try: 
-                ax.plot(elec_x, self.elec_z[~iremote],'ko')
+#                ax.plot(elec_x, self.elec_z[~iremote],'ko')
+                radius = np.min(np.diff(elec_x))/7
+                circles = [plt.Circle((xi,yi), radius=radius) for xi,yi in zip(elec_x, self.elec_z[~iremote])]
+                ax.add_collection(PatchCollection(circles, color='k'))
             except AttributeError:
                 print("no electrodes in mesh object to plot")
 

src/ui.py
@@ -1199,19 +1199,23 @@ class App(QMainWindow):
         tabImportingDataLayout.addLayout(metaLayout, 40)
         
         # update pseudo-sections due to electrodes update
+        self.tempElec = None
         def updateElec():
             try:
                 elec = elecTable.getTable()
-                self.r2.setElec(elec)
-                self.r2.mesh = None
-                mwMesh.clear()
-                plotPseudo()
-                plotManualFiltering()
-                if self.r2.typ[0] == 'c':
-                    plotPseudoIP()
+                if self.tempElec is None or np.sum(elec-self.tempElec) != 0:
+                    self.tempElec = elec
+                    self.r2.setElec(elec)
+                    self.r2.mesh = None
+                    mwMesh.clear()
+                    if len(self.r2.surveys) > 0:
+                        plotPseudo()
+                        plotManualFiltering()
+                        if self.r2.typ[0] == 'c':
+                            plotPseudoIP()
             except Exception as e:
                 errorDump('Error updating pseudosection: ' + e)
-        
+    
         # tab focus events functions
         self.currentImportTab = 0
         self.currentTab = 0
@@ -1223,7 +1227,7 @@ class App(QMainWindow):
             if index != 0 and self.currentImportTab == 1:
                 # elec tab still visible but we moved out to another
                 # higher level tab
-                updateElec()
+                tabImporting.setCurrentIndex(0)
             self.currentTab = index
         tabImporting.currentChanged.connect(logImportTab)
         tabs.currentChanged.connect(logTab)