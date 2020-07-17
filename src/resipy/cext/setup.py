2d001813e0dc5222fbceeccc7ac67f22688e9b04
Jimmy Boyd <j.boyd1@lancaster.ac.uk>
2020-07-17 16:39:09 +0100

    Dropping openmp support on windows and macOS (for now)

--- src/resipy/cext/setup.py
+++ src/resipy/cext/setup.py
@@ -2,23 +2,25 @@
 from distutils.core import setup, Extension 
 from Cython.Build import cythonize
 import numpy as np
-import platorm
+import platform
 
-if platorm.system() == 'Linux':
-    earg = '-fopenmp'
-elif platorm.system() == 'Windows':
-    earg = '/openmp'
+if platform.system() == 'Linux': #open mp support on linux only 
+    ext_modules = [
+        Extension(
+            "meshCalc",
+            ["meshCalc.pyx"],
+            extra_compile_args=['-fopenmp'],
+            extra_link_args=['-fopenmp'],
+        )
+    ]
 else:
-    earg = ''
-    
-ext_modules = [
-    Extension(
-        "meshCalc",
-        ["meshCalc.pyx"],
-        extra_compile_args=['-fopenmp'],
-        extra_link_args=['-fopenmp'],
-    )
-]
+    ext_modules = [
+        Extension(
+            "meshCalc",
+            ["meshCalc.pyx"],
+        )
+    ]
+
 setup(
     ext_modules=cythonize(ext_modules),
     include_dirs=[np.get_include()]
