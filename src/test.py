47973be22fb0339051fd9365c0adffc791c3edac
Jimmy B <j.boyd1@lancaster.ac.uk>
2021-01-26 15:43:27 +0000

    Should fix refining problems

--- src/test.py
+++ src/test.py
@@ -523,7 +523,7 @@ k = Project(typ='R3t')
 k.createSurvey(testdir + 'dc-3d/protocol.dat', ftype='ProtocolDC')
 k.importElec(testdir + 'dc-3d/elec.csv')
 # k.showPseudo(threed=True) # only tested in pyvista setup   
-k.createMesh(cl=1.5)#, interp_method='bilinear', cl_factor=20, cln_factor=500)
+k.createMesh(cl=4, refine=1)#, interp_method='bilinear', cl_factor=20, cln_factor=500)
 
 k.createSequence()
 #k.err = True
@@ -547,7 +547,7 @@ t0 = time.time()
 k = Project(typ='R3t')
 k.createSurvey(testdir + 'dc-3d/protocol.dat', ftype='ProtocolDC')
 k.importElec(testdir + 'dc-3d/elec.csv')
-k.importMesh(testdir + 'mesh/coarse3D.vtk')
+k.importMesh(testdir + 'mesh/mesh3D.vtk')
 rmesh = k.mesh.refine() # test refining mesh 
 k.addFlatError()
 k.invert()
