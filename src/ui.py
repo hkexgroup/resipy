cc716c28171ed8d171e7151101d4bacb1424f490
Sina Saneiyan <s.sanei@gmail.com>
2021-02-04 08:48:46 -0500

    recompile meshCalc for mac - solve issu with 'resipy.cext.meshCalc' has no attribute 'sortNeigh'

--- src/ui.py
+++ src/ui.py
@@ -3459,7 +3459,7 @@ class App(QMainWindow):
                 return
             fname, _ = QFileDialog.getOpenFileName(self.tabImportingData,'Open File', self.datadir)
             if fname != '':
-                try:
+                # try:
                     self.project.importMesh(fname)
                     self.writeLog('k.importMesh("{:s}")'.format(fname))
                     print('mesh imported ... now displaying ... ')
@@ -3470,8 +3470,8 @@ class App(QMainWindow):
                         self.mwMesh3D.plot(self.project.showMesh, threed=True)
                     self.writeLog('k.showMesh()')
                     self.meshOutputStack.setCurrentIndex(2)
-                except Exception as e:
-                    self.errorDump('Error importing mesh' + str(e))
+                # except Exception as e:
+                #     self.errorDump('Error importing mesh' + str(e))
         self.importCustomMeshBtn = QPushButton('Import Custom Mesh')
         self.importCustomMeshBtn.setFixedWidth(150)
         self.importCustomMeshBtn.clicked.connect(importCustomMeshFunc)
